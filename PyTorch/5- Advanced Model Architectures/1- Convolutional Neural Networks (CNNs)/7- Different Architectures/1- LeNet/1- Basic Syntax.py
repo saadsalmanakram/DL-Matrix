048734b3eee3cfdba42a5ad81fbd0c711c944717
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 18:51:34 +0500

    Create 1- Basic Syntax.py

---
+++ PyTorch/5- Advanced Model Architectures/1- Convolutional Neural Networks (CNNs)/7- Different Architectures/1- LeNet/1- Basic Syntax.py
@@ -0,0 +1,19 @@
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16*4*4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.max_pool2d(x, 2)
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.max_pool2d(x, 2)
+        x = x.view(-1, 16*4*4)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
