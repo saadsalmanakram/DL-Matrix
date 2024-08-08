3339c8209e210fff457df7c86f25019e064f4e95
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 15:17:24 +0500

    update

---
+++ PyTorch/2- PyTorch Fundamentals/1- Tensors/1- Creating Tensors/1- Creating Tensors Code.py
@@ -0,0 +1,13 @@
+import torch
+
+# Create a tensor from a list
+tensor_from_list = torch.tensor([1, 2, 3, 4])
+
+# Create a tensor with zeros
+tensor_zeros = torch.zeros((2, 3))
+
+# Create a tensor with ones
+tensor_ones = torch.ones((2, 3))
+
+# Create a tensor with random values
+tensor_random = torch.rand((2, 3))
