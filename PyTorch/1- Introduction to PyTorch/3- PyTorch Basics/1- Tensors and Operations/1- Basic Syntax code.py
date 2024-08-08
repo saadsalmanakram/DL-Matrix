682b2b963fd1fecf7b3f68f77d2969fbed8ec88b
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 15:04:08 +0500

    Create 1- Basic Syntax code.py

---
+++ PyTorch/1- Introduction to PyTorch/3- PyTorch Basics/1- Tensors and Operations/1- Basic Syntax code.py
@@ -0,0 +1,8 @@
+import torch
+
+# Create a tensor
+tensor = torch.tensor(data, dtype=torch.float32)
+
+# Operations
+tensor_sum = torch.add(tensor1, tensor2)
+tensor_product = torch.matmul(tensor1, tensor2)
