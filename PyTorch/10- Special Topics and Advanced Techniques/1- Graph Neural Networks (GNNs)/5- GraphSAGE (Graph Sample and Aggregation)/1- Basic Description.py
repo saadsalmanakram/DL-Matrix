19da4d833dc89a2becef35fa8aef5f1b059faa3d
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 23:41:15 +0500

    Create 1- Basic Description.py

---
+++ PyTorch/10- Special Topics and Advanced Techniques/1- Graph Neural Networks (GNNs)/5- GraphSAGE (Graph Sample and Aggregation)/1- Basic Description.py
@@ -0,0 +1,13 @@
+from torch_geometric.nn import SAGEConv
+
+class GraphSAGE(nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(in_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, out_channels)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return x
