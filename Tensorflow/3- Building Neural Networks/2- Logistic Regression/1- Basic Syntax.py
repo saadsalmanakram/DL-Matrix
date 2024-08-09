e44ef0c9824cebdcf7010ff0f5cedb5bcf9927d3
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-09 08:27:37 +0500

    Create 1- Basic Syntax.py

---
+++ Tensorflow/3- Building Neural Networks/2- Logistic Regression/1- Basic Syntax.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+
+# Sample data
+X = np.array([[0], [1]], dtype=float)
+y = np.array([[0], [1]], dtype=float)
+
+# Define the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=1, activation='sigmoid', input_shape=[1])
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X, y, epochs=500)
+
+# Predictions
+print(model.predict([0.5]))
