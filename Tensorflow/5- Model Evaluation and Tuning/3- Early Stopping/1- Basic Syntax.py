b7643da6c650ac12b55daf07a7b8281fdbd93fda
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-09 09:00:42 +0500

    Create 1- Basic Syntax.py

---
+++ Tensorflow/5- Model Evaluation and Tuning/3- Early Stopping/1- Basic Syntax.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.callbacks import EarlyStopping
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+model.fit(X_train, y_train, validation_split=0.2, epochs=50, callbacks=[early_stopping])
